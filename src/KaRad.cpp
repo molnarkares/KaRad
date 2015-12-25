6b7bb34d2489185ac6caec1ca9936748d92091b1
Kares <molnarkaroly@hotmail.com>
2015-12-25 19:37:07 +0100

    Updated with OTA and some sinitial ideas on cpm

--- src/KaRad.cpp
+++ src/KaRad.cpp
@@ -23,53 +23,105 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
+extern "C" {
+#include "ets_sys.h"
+#include "os_type.h"
+#include "osapi.h"
+#include "mem.h"
+#include "user_interface.h"
+#include "cont.h"
+}
 
 ADC_MODE(ADC_VCC);
 
-#define USE_SERIAL Serial
-
-const int led = 13;
+const int led_pin = BUILTIN_LED;//or 13;
+const int beep_pin = D1; // GPIO05
+char ap_hostname[] = "KaRad1";
+#define CPM_BEEP_LIMIT 100
 
 
 void configModeCallback () {
-	  digitalWrite(led, 1); //turn LED on if AP mode i.e. configuration is needed
+	  digitalWrite(led_pin, LOW); //turn LED on if AP mode i.e. configuration is needed
 }
 
-enum sensorState_t {standalone,connected};
+enum sensorState_t {
+	sensor_standalone,
+	sensor_connected,
+	sensor_update};
+
+bool beeping = false;
+
 sensorState_t sensorState;
+
+uint16_t cpm,cpm_u;
+
+unsigned long elapsedms;
+
+bool sendUpdate(uint16_t voltage, uint16_t cpm);
+void beep(void);
+void cpmHandler(void);
+
+WiFiManager wifiManager;
 
 void setup() {
 #ifdef WDT_ENABLED
   ESP.wdtEnable(10000);
 #endif
 
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+  pinMode(led_pin, OUTPUT);
+  digitalWrite(led_pin, HIGH);
 
-  USE_SERIAL.begin(115200);
 
-	//WiFiManager
-	//Local intialization. Once its business is done, there is no need to keep it around
-	WiFiManager wifiManager;
+  analogWriteFreq(2000);
+  pinMode(beep_pin,OUTPUT);
+
+  Serial.begin(115200);
+
 	//reset saved settings
-	wifiManager.resetSettings();
-	//wifiManager.setDebugOutput(false);
+//	wifiManager.resetSettings();
 
 
 	wifiManager.setTimeout(180);
 
 	wifiManager.setAPCallback(configModeCallback);
 
-	if(wifiManager.autoConnect("KaRad1 AP")) {
-		//if you get here you have connected to the WiFi
-		USE_SERIAL.println("connected... :)");
-		sensorState = connected;
+
+	ArduinoOTA.onStart([]() {
+	    sensorState = sensor_update;
+	    beep();
+	  });
+	ArduinoOTA.onEnd([]() {
+	    beep();
+	    delay(100);
+	    beep();
+	  });
+	ArduinoOTA.onError([](ota_error_t error) {
+	    Serial.printf("Error[%u]: ", error);
+	    sensorState = sensor_connected;
+	  });
+
+	if(wifiManager.autoConnect(ap_hostname)) {
+	//if you get here you have connected to the WiFi
+		Serial.println("connected... :)");
+		sensorState = sensor_connected;
+		ArduinoOTA.setHostname(ap_hostname);
+		ArduinoOTA.begin();
+		wifiManager.setTimeout(20); //significantly shorter timeout for runtime reconnects
 	}else {
-		//was unable to connect and timeout
-		sensorState = standalone;
+	//was unable to connect and timeout
+		Serial.println("No connection was possible within 3 minutes... :(");
+		sensorState = sensor_standalone;
+		beeping = true;
 	}
-	digitalWrite(led, 0); // turn off LED
+	digitalWrite(led_pin, HIGH); // turn off LED
+	elapsedms = millis();
+	cpm=0;
+	cpm_u=0;
+
 }
 
 void loop() {
@@ -78,5 +130,54 @@ void loop() {
     ESP.wdtFeed();
 #endif
 
-    uint16_t voltage = ESP.getVcc();
+    if(sensorState == sensor_connected) {
+    	uint16_t voltage = ESP.getVcc();
+    	if(!sendUpdate(voltage,cpm)) {
+    		beeping = true;
+    	}
+    }
+
+    if(sensorState == sensor_update) {
+    	ArduinoOTA.handle();
+    }else {
+    	delay(60000);
+    }
+
+}
+
+bool sendUpdate(uint16_t voltage, uint16_t cpm) {
+	if(wifiManager.autoConnect(ap_hostname)) {
+		return true;
+	}else {
+		return false;
+	}
+}
+
+
+void cpmHandler(void) {
+	char rtcStore[2];
+	system_rtc_mem_read(64, rtcStore, 2);
+
+	unsigned long msnow = millis();
+	unsigned long msdiff;
+	cpm++;
+	if(msnow < elapsedms) { //overrun
+		msdiff = elapsedms - msnow;
+	}else {
+		msdiff = msnow - elapsedms;
+	}
+
+	if(msdiff >= 60000) {
+
+		cpm_u = cpm;
+	}
+	if(beeping || (cpm > CPM_BEEP_LIMIT)) {
+		beep();
+	}
+}
+
+void beep(void) {
+	analogWrite(beep_pin,PWMRANGE>>1);
+	delay(10);
+	analogWrite(beep_pin,0);
 }
